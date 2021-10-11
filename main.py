91cf6d6df5f4b14d46a03ca884e8b3e3c5f6f32a
JakeVestal <jmvestal@gmail.com>
2021-10-11 13:36:17 -0400

    added pkg

---
+++ main.py
@@ -0,0 +1,9 @@
+
+# Import Multiplication from your library
+from financier import Multiplication
+
+# Instantiate a Multiplication object
+multiplication = Multiplication(2)
+
+# Call the multiply method
+print(multiplication.multiply(5))
