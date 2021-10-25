eb1a0890d408c62962815ecfd18e6a2b4009f6fa
Owen Astrachangit <sophie.rubin@duke.edu>
2021-10-25 12:10:20 -0400

    commit new scripts

--- tests/calc_market_portfolio_tests.py
+++ tests/calc_market_portfolio_tests.py
@@ -1,10 +1,16 @@
+import os
 import unittest
 import pandas as pd
 from financier import calc_market_portfolio
 
 class mp_testcase(unittest.TestCase):
+    print("----")
+    print(os.getcwd())
     df = pd.read_csv('../tests/data/10stocks.csv')
     def test_noshorts(self):
         calc_market_portfolio(df,allow_short="No")
     def test_shorts(self):
         calc_market_portfolio(df,allow_short="Yes")
+
+if __name__ == '__main__':
+    unittest.main()
